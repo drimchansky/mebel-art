9912f6c791c4416aea21748a4b6a511b3f71cc27
Nikita Chernov <drimchansky@gmail.com>
2020-12-06 14:10:05 +0300

    Try this way

--- src/html.js
+++ src/html.js
@@ -26,15 +26,17 @@ export default function HTML(props) {
         <meta name="msapplication-TileColor" content="#1c5796" />
         <meta name="theme-color" content="#1c5796" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM2WV44H9X" />
+
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KGPGF2V');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-LM2WV44H9X');
         `,
           }}
         />
@@ -48,20 +50,6 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-        {/* Google Tag Manager */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-            <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KGPGF2V"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"></iframe>
-        `,
-          }}
-        />
-        {/* End Google Tag Manager  */}
-
         {props.preBodyComponents}
         <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
