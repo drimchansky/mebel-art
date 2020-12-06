d9e334a51c584de65cd57f3628ccbb4b200efe2f
Nikita Chernov <drimchansky@gmail.com>
2020-12-06 13:39:59 +0300

    Add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,27 +47,5 @@ module.exports = {
     `@contentful/gatsby-transformer-contentful-richtext`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'G-LM2WV44H9X',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-      },
-    },
   ],
 }
--- src/html.js
+++ src/html.js
@@ -26,6 +26,20 @@ export default function HTML(props) {
         <meta name="msapplication-TileColor" content="#1c5796" />
         <meta name="theme-color" content="#1c5796" />
 
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KGPGF2V');
+        `,
+          }}
+        />
+        {/* End Google Tag Manager  */}
+
         <meta
           name="google-site-verification"
           content="G4uA9d9gfAG6-nqFgZLBj_VXSZb-M7ur2BFS9rKymQc"
@@ -34,6 +48,14 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGPGF2V"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"></iframe>
+        </noscript>
+
         {props.preBodyComponents}
         <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
